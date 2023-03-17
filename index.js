ff77447d7e320ab65cd0a313ed74c13b7a8ea61b
Joseph McCafferty <mcafferty.j11@gmail.com>
2023-03-17 17:51:56 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(name, value){
+    let myArray=[]
+    for(let i=0; i < name.length; i++) {
+        myArray.push(`Thank you, ${name[i]}, for the wonderful ${value} gift!`)}
+    return myArray
+}
+
+function countDown(number) {
+    while (number >=0) {
+        console.log (number)
+        number --
+    }
+}
